aaeeeab5efdfd78b6f140af1a793e2e199fb24bc
zy <zengyuan@keywa.com>
2021-03-23 11:25:48 +0800

    3-父子组件间的通信：$parent/$children

--- 1-component-communication/src/views/Parent.vue
+++ 1-component-communication/src/views/Parent.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>This is a Parent Page</h1>
+    <button @click="handleClick">get Child</button><span>{{txt}}</span>
     <ParentChild></ParentChild>
   </div>
 </template>
@@ -9,6 +10,19 @@ import ParentChild from '@/components/ParentChild.vue'
 export default {
   components: {
     ParentChild,
+  },
+  data() {
+    return {
+      txt: ''
+    }
+  },
+  methods: {
+    handleClick() {
+      this.txt = this.$children[0].txt
+    },
+    changeTxt() {
+      this.txt = 'change from child'
+    }
   },
 }
 </script>
